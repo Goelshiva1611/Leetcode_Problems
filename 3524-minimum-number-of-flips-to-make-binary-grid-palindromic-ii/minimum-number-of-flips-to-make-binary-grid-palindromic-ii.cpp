59eb1f0fda5461c11a2c35b0b22d33aaf3e968a4
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-08-04 01:48:24 +0530

    Time: 388 ms (50.00%) | Memory: 193.7 MB (60.00%) - LeetSync

--- 3524-minimum-number-of-flips-to-make-binary-grid-palindromic-ii/minimum-number-of-flips-to-make-binary-grid-palindromic-ii.cpp
+++ 3524-minimum-number-of-flips-to-make-binary-grid-palindromic-ii/minimum-number-of-flips-to-make-binary-grid-palindromic-ii.cpp
@@ -1,54 +1,71 @@
-#include <vector>
-#include <iostream>
-
-using namespace std;
-
 class Solution {
 public:
     int minFlips(vector<vector<int>>& grid) {
         int n = grid.size();
-        int m = grid[0].size();
         int count = 0;
-
-      
-
-        // General case for grids larger than 1x1
+        int t = 0;
+        int m = grid[0].size(); // column size;
         for (int i = 0; i < n / 2; i++) {
+            count = 0;
             for (int j = 0; j < m / 2; j++) {
-                int currOne = grid[i][j] + grid[i][m - j - 1] + grid[n - 1 - i][j] + grid[n - 1 - i][m - j - 1];
-                if (currOne == 1 || currOne == 3) {
+                if (grid[i][j] == 1) {
                     count++;
-                } else if (currOne == 2) {
-                    count += 2;
                 }
-            }
-        }
+                if (grid[i][m - j - 1] == 1) {
+                    count++;
+                }
+                if (grid[n - i - 1][j] == 1) {
+                    count++;
+                }
+                if (grid[n - i - 1][m - j - 1] == 1) {
+                    count++;
+                }
+                cout<<count;
 
-        if(n%2==0 && m%2==0) return count;
-        int palinOnes=0;
-        int onechange=0;
-        if(m%2==1){
-            for(int i=0;i<n/2;i++){
-                if(grid[i][m/2]==1 && grid[n-i-1][m/2]==1) palinOnes++;
-                if(grid[i][m/2]!= grid[n-i-1][m/2]){
-                    count++;
-                    onechange++;
+                if (count % 4 != 0) {
+                    if (count == 1 || count == 3) {
+                        t++;
+                    }
+                    if (count == 2) {
+                        t =t+ 2;
+                    }
+                }
+                count=0;
+            }
+        }
+        cout << t;
+        int changes = 0;
+        int c = 0;
+        if (n % 2 == 1) {
+            for (int i = 0; i < m / 2; i++) {
+                if (grid[n / 2][i] != grid[n / 2][m - i - 1]) {
+                    t++;
+                    changes++;
+                } else {
+                    if (grid[n / 2][i] == 1 && grid[n / 2][m - i - 1] == 1)
+                        c = c + 2;
                 }
             }
         }
-        if(n%2==1){
-            for(int i=0;i<m/2;i++){
-                if(grid[n/2][i]==1 && grid[n/2][m-i-1]==1) palinOnes++;
-                if(grid[n/2][i]!= grid[n/2][m-i-1]){
-                    count++;
-                    onechange++;
+        if (m % 2 == 1) {
+            for (int i = 0; i < n / 2; i++) {
+                if (grid[i][m / 2] != grid[n - i - 1][m / 2]) {
+                    t++;
+                    changes++;
+                } else {
+                    if (grid[i][m / 2] == 1 && grid[n-i-1][m/2]==1)
+                        c = c + 2;
                 }
             }
         }
-        if(palinOnes%2==1){
-            if(onechange==0) count+=2;
+        if (m % 2 == 1 && n % 2 == 1 && grid[n / 2][m / 2] == 1) {
+            t++;
         }
-        if(n%2==1 && m%2==1 && grid[n/2][m/2]==1) count++;
-        return count;
+        if (c % 4 == 2) {
+            if (changes == 0) {
+                t = t + 2;
+            }
+        }
+        return t;
     }
 };
