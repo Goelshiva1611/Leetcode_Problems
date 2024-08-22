f2a9c9e8dd353cf5c06e39f2ba75e78032b2a73d
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-08-22 13:20:03 +0530

    Time: 2 ms (50.07%) | Memory: 7.6 MB (8.34%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
-    string decimalTobinary(int n) {
-        string s = "";
-        while (n) {
-            s = s + to_string(n % 2);
-            n = n / 2;
-        }
-        return s;
-    }
-    int binaryTodecimal(string s) {
-        int n = s.size();
-        int num = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            num = num + (s[i] - '0') * pow(2, i);
-        }
-        return num;
-    }
     int findComplement(int num) {
-        string s = decimalTobinary(num);
-        for (int i = 0; i < s.size(); i++) {
-            (s[i] == '1') ? s[i] = '0' : s[i] = '1';
+        int ans = 0;
+        int i = 0;
+        while (num) {
+            (num % 2 == 1) ?: ans += pow(2, i);
+            num /= 2;
+            i++;
         }
-        return binaryTodecimal(s);
+        return ans;
     }
 };
