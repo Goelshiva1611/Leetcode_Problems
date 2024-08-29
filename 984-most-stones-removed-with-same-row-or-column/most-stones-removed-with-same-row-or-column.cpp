a701bbde9df220da6dd3a2c433eaa03a2f96afb4
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-08-29 18:14:28 +0530

    Time: 232 ms (5.07%) | Memory: 18 MB (91.85%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    // most stones removed from same row and same column;
+    // on a 2D plane
+    // stones of length n where stones[i]=[xi,yi];
+    int n;
+    void dfs(int index, vector<bool> &visited, vector<vector<int>> &stones) {
+        visited[index] = true;
+        for (int i = 0; i < n; i++) {
+            int row = stones[i][0];
+            int col = stones[i][1];
+            if (visited[i]==false &&
+                (stones[index][0] == row || stones[index][1] == col)) {
+                dfs(i, visited, stones);
+            }
+        }
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        int group = 0;
+        n = stones.size();
+        vector<bool> visited(n, false);
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i]==false) {
+                dfs(i, visited, stones);
+                group++;
+                cout<<"hello";
+            }
+        }
+        return n - group;
+    }
+};
