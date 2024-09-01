5438c2ef57ab8dfa4b8f5862ded447f8396a176f
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-09-01 17:08:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (15.85%) - LeetSync

--- 2602-maximum-enemy-forts-that-can-be-captured/maximum-enemy-forts-that-can-be-captured.cpp
+++ 2602-maximum-enemy-forts-that-can-be-captured/maximum-enemy-forts-that-can-be-captured.cpp
@@ -5,54 +5,38 @@ public:
         int n = forts.size();
         for (int i = 0; i < n; i++) {
             if (forts[i] == 1) {
-                // start attacking kyuli mera fort hai
                 int count = 0;
                 bool flag = false;
                 for (int j = i + 1; j < n; j++) {
-                    if (forts[j] == -1 ) {
+                    if (forts[j] == -1) {
                         flag = true;
                         break;
                     }
-                    if(forts[j]==1)
-                    {
+                    if (forts[j] == 1)
                         break;
-                    }
-                    if (forts[j] == 0) {
+                    if (forts[j] == 0)
                         count++;
-                    }
                 }
-                if (flag == true) {
+                if (flag == true)
                     v.push_back(count);
-                }
             }
         }
         for (int i = n - 1; i >= 0; i--) {
             if (forts[i] == 1) {
-                // start attacking kyuli mera fort hai
-                cout<<i;
                 int count = 0;
                 bool flag = false;
                 for (int j = i - 1; j >= 0; j--) {
                     if (forts[j] == -1) {
                         flag = true;
-                        cout<<"tt";
-                        cout<<endl;
                         break;
                     }
-                    if(forts[j]==1)
-                    {
-                        cout<<j;
-                        cout<<"jjd";
-                        cout<<endl;
+                    if (forts[j] == 1)
                         break;
-                    }
-                    if (forts[j] == 0) {
+                    if (forts[j] == 0)
                         count++;
-                    }
                 }
-                if (flag == true) {
+                if (flag == true)
                     v.push_back(count);
-                }
             }
         }
         if (v.size() > 0) {
