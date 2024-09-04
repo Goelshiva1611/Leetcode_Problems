daf6cb1f6b5743bb272060a33e142da671bf39d9
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-09-04 17:10:25 +0530

    Time: 83 ms (51.11%) | Memory: 38 MB (70.72%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.cpp
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -2,16 +2,16 @@
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        int newx = 0;
+        int newy = 0;
         int n = commands.size();
         set<pair<int, int>> st;
+        vector<int> direction(4, 0);
+        direction[0] = 1;
+        vector<int> distance;
         for (int i = 0; i < obstacles.size(); i++) {
             st.insert({obstacles[i][0], obstacles[i][1]});
         }
-        vector<int> direction(4, 0);
-        direction[0] = 1;
-        int newx = 0;
-        int newy = 0;
-        vector<int> distance;
         for (int i = 0; i < n; i++) {
             if (commands[i] >= 1 && commands[i] <= 9) {
                 if (direction[0] == 1) { // north
@@ -22,18 +22,15 @@ public:
                             break;
                         }
                     }
-                    cout << "T";
                     distance.push_back(newx * newx + newy * newy);
                     continue;
                 }
                 if (direction[1] == 1) // east
                 {
-                    cout << "xscdcdcdcd";
                     for (int j = 0; j < commands[i]; j++) {
                         newx += 1;
                         if (st.find({newx, newy}) != st.end()) {
                             newx = newx - 1;
-                            cout << "heheheh";
                             break;
                         }
                     }
@@ -44,7 +41,6 @@ public:
                 {
                     for (int j = 0; j < commands[i]; j++) {
                         newy -= 1;
-                        cout << "jhjfkj";
                         if (st.find({newx, newy}) != st.end()) {
                             newy = newy + 1;
                             break;
@@ -65,57 +61,27 @@ public:
                     distance.push_back(newx * newx + newy * newy);
                 }
             }
-            cout << "jcdk";
             if (commands[i] == -2) {
-                if (direction[0] == 1) {
-                    direction[0] = 0;
-                    direction[3] = 1;
-                    continue;
-                }
-                if (direction[3] == 1) {
-                    direction[3] = 0;
-                    direction[2] = 1;
-                    continue;
-                }
-                if (direction[2] == 1) {
-                    direction[2] = 0;
-
-                    direction[1] = 1;
-                    continue;
-                }
-                if (direction[1] == 1) {
-                    direction[1] = 0;
-                    direction[0] = 1;
-                    continue;
+                for (int i = 0; i < direction.size(); i++) {
+                    if (direction[i] == 1) {
+                        direction = {0, 0, 0, 0};
+                        direction[(i + 3) % 4] = 1;
+                        break;
+                    }
                 }
             }
             if (commands[i] == -1) {
-                cout << "d";
-                if (direction[0] == 1) {
-                    direction[0] = 0;
-                    direction[1] = 1;
-                    continue;
-                }
-                cout << "lllll";
-                if (direction[1] == 1) {
-                    direction[1] = 0;
-                    direction[2] = 1;
-                    cout << "jjh";
-                    continue;
-                }
-                if (direction[2] == 1) {
-                    direction[2] = 0;
-                    direction[3] = 1;
-                    continue;
-                }
-                if (direction[3] == 1) {
-                    direction[3] = 0;
-                    direction[0] = 1;
-                    continue;
+                for (int i = 0; i < direction.size(); i++) {
+                    if (direction[i] == 1) {
+                        direction = {0, 0, 0, 0};
+                        direction[(i + 1) % 4] = 1;
+                        break;
+                    }
                 }
             }
         }
-        sort(distance.rbegin(), distance.rend());
-        return distance[0];
+        nth_element(begin(distance), begin(distance) + distance.size() - 1,
+                    end(distance));
+        return distance[distance.size() - 1];
     }
 };
