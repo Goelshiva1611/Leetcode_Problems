d81ebc27106a21190a1cda808682f2bc89d07426
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-09-06 14:43:12 +0530

    Time: 4 ms (84.77%) | Memory: 17.3 MB (38.34%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int n;
     int m;
-    int color1;
-    int c;
+    int color_To_do;
+    int color_initial;
     void dfs(int i, int j, vector<vector<int>>& image,
              vector<vector<int>>& visited) {
-        if (i < 0 || i >= n || j < 0 || j >= m) {
+
+        if (i < 0 || i >= n || j < 0 || j >= m || visited[i][j] ||
+            image[i][j] != color_initial)
             return;
-        }
-        if (visited[i][j] || image[i][j] != color1) {
-            return;
-        }
         visited[i][j] = 1;
-        image[i][j] = c;
+        image[i][j] = color_To_do;
         dfs(i - 1, j, image, visited);
         dfs(i, j + 1, image, visited);
         dfs(i, j - 1, image, visited);
@@ -23,12 +21,11 @@ public:
                                   int color) {
         n = image.size();
         m = image[0].size();
-        c = color;
+        color_To_do = color;
         vector<vector<int>> visited(n, vector<int>(m, 0));
-        color1 = image[sr][sc];
-        if (color1 == color) {
+        color_initial = image[sr][sc];
+        if (color_initial == color_To_do)
             return image;
-        }
         dfs(sr, sc, image, visited);
         return image;
     }
