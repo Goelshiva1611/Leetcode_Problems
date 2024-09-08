67c65bad40a4a6590d7c6ab878d7525ac0bc37f5
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-09-08 11:01:09 +0530

    Time: 4 ms (73.95%) | Memory: 13.8 MB (61.52%) - LeetSync

---
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.cpp
@@ -0,0 +1,38 @@
+
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        int count = 0;
+        ListNode* dummy = head;
+        while (dummy != nullptr) {
+            dummy = dummy->next;
+            count++;
+        }
+        int quo = count / k;
+        int remainder = count % k;
+        vector<ListNode*> v(k);
+        ListNode* d = head;
+        int q = 0;
+        int t = quo;
+        for (int i = 0; i < k; i++) {
+            d = head;
+            if (remainder <= 0)
+                t = quo - 1;
+            else
+                t = quo;
+            while (t-- && head != nullptr) {
+                head = head->next;
+            }
+            if (head != nullptr) {
+                ListNode* newhead = head->next;
+                head->next = nullptr;
+                v[q++] = d;
+                head = newhead;
+            } else {
+                v[q] = d;
+            }
+            remainder--;
+        }
+        return v;
+    }
+};
