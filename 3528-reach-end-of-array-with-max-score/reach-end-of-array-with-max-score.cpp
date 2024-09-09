f4a8f37cdcc908c2908d9b89afff640d42173c42
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-09-09 12:31:21 +0530

    Time: 192 ms (100.00%) | Memory: 171.5 MB (80.00%) - LeetSync

--- 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.cpp
+++ 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     long long findMaximumScore(vector<int>& nums) {
+        long long n = nums.size();
+        long long i = 1;
+        long long start = nums[0];
         long long ans = 0;
-        int curr = nums[0];
-        for (int i = 1; i < nums.size(); i++) {
-            ans += curr;
-            curr = max(curr, nums[i]);
+        long long p = 0;
+        while (i != n) {
+            bool flag = true;
+            if (start < nums[i]) {
+                ans += (i - p) * start;
+                start = nums[i];
+                p = i;
+                flag = false;
+            }
+            i++;
+            if (i == n && flag == true) {
+                ans += (i - p - 1) * start;
+                break;
+            }
         }
         return ans;
     }
