dd85bb15c1ba55e6ca769ddb104de4bf6cfde6bd
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-09-09 11:41:13 +0530

    Time: 174 ms (28.33%) | Memory: 130.6 MB (65.45%) - LeetSync

--- 2411-spiral-matrix-iv/spiral-matrix-iv.cpp
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
-        vector<int> a;
-        while (head) {
-            a.push_back(head->val);
-            head = head->next;
-        }
         vector<vector<int>> matrix(m, vector<int>(n, -1));
         vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         int rstart = 0, cstart = 0;
         int left = 0, right = n - 1, top = 0, bottom = m - 1;
         int d = 0;
         int i = 0;
-        while (i < a.size()) {
+        while (head != nullptr) {
             if (d == 0) {
-                for (int j = left; j <= right && i < a.size(); j++) {
-                    matrix[top][j] = a[i];
-                    i++;
+                for (int j = left; j <= right && head != nullptr; j++) {
+                    matrix[top][j] = head->val;
+                    head = head->next;
                 }
                 top++;
             } else if (d == 1) {
-                for (int j = top; j <= bottom && i < a.size(); j++) {
-                    matrix[j][right] = a[i];
-                    i++;
+                for (int j = top; j <= bottom && head != nullptr; j++) {
+                    matrix[j][right] = head->val;
+                    head = head->next;
                 }
                 right--;
             } else if (d == 2) {
-                for (int j = right; j >= left && i < a.size(); j--) {
-                    matrix[bottom][j] = a[i];
-                    i++;
+                for (int j = right; j >= left && head != nullptr; j--) {
+                    matrix[bottom][j] = head->val;
+                    head = head->next;
                 }
                 bottom--;
             } else if (d == 3) {
-                for (int j = bottom; j >= top && i < a.size(); j--) {
-                    matrix[j][left] = a[i];
-                    i++;
+                for (int j = bottom; j >= top && head != nullptr; j--) {
+                    matrix[j][left] = head->val;
+                    head = head->next;
                 }
                 left++;
             }
-
             d = (d + 1) % 4;
         }
         return matrix;
