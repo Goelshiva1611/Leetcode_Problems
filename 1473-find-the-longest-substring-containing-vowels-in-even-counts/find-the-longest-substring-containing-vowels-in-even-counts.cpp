67112a748247f96ca75c5d5957cc2f0170fb9ff6
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-09-16 02:41:51 +0530

    Time: 125 ms (9.40%) | Memory: 18.9 MB (33.03%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -2,36 +2,29 @@ class Solution {
 public:
     int findTheLongestSubstring(string s) {
         string currentstate = "00000";
-        int n = s.size();
-        int a = 0;
         unordered_map<string, int> mp;
         mp[currentstate] = -1;
         int result = 0;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == 'a') {
-                a = currentstate[0] - '0';
-                currentstate[0] = ((a + 1) % 2 == 0) ? '0' : '1';
-            } else if (s[i] == 'e') {
-                a = currentstate[1] - '0';
-                cout << a;
-                currentstate[1] = ((a + 1) % 2 == 0) ? '0' : '1';
-            } else if (s[i] == 'i') {
-                a = currentstate[2] - '0';
-                currentstate[2] = ((a + 1) % 2 == 0) ? '0' : '1';
-            } else if (s[i] == 'o') {
-                a = currentstate[3] - '0';
-                currentstate[3] = ((a + 1) % 2 == 0) ? '0' : '1';
-            } else if (s[i] == 'u') {
-                a = currentstate[4] - '0';
-                currentstate[4] = ((a + 1) % 2 == 0) ? '0' : '1';
-            }
-            cout << currentstate;
-            if (mp.find(currentstate) != mp.end()) {
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'a')
+                currentstate[0] =
+                    (((currentstate[0] - '0') + 1) % 2 == 0) ? '0' : '1';
+            else if (s[i] == 'e')
+                currentstate[1] =
+                    (((currentstate[1] - '0') + 1) % 2 == 0) ? '0' : '1';
+            else if (s[i] == 'i')
+                currentstate[2] =
+                    (((currentstate[2] - '0') + 1) % 2 == 0) ? '0' : '1';
+            else if (s[i] == 'o')
+                currentstate[3] =
+                    (((currentstate[3] - '0') + 1) % 2 == 0) ? '0' : '1';
+            else if (s[i] == 'u')
+                currentstate[4] =
+                    (((currentstate[4] - '0') + 1) % 2 == 0) ? '0' : '1';
+            if (mp.find(currentstate) != mp.end())
                 result = max(result, abs(mp[currentstate] - i));
-            } else {
-                cout << i;
+            else
                 mp[currentstate] = i;
-            }
         }
         return result;
     }
