770e22f9b6e9651e8fb49a338aa14e052755443e
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-09-25 02:56:10 +0530

    Time: 11 ms (5.38%) | Memory: 15.5 MB (8.51%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int n = strs.size();
-        string p = strs[0];
+        string q = "", ans = "";
         set<string> st;
-        string q = "";
-        for (int i = 0; i < p.size(); i++) {
-            q = q + p[i];
-            cout << q;
+        for (int i = 0; i < strs[0].size(); i++) {
+            q = q + strs[0][i];
             st.insert(q);
         }
-        int maxi = 0;
-        string ans = "";
         for (auto it : st) {
             string p = it;
-            for (int i = 1; i < n; i++) {
-                if (strs[i].substr(0, p.size()) != p) {
+            for (int i = 1; i < strs.size(); i++)
+                if (strs[i].substr(0, p.size()) != p)
                     return ans;
-                }
-            }
             ans = p;
         }
         return ans;
