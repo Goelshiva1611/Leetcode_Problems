8e7236a80acc4cc24e5bf1fccfe858ee667b08e9
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-09-28 01:06:42 +0530

    Time: 72 ms (73.60%) | Memory: 41.4 MB (99.42%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.cpp
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -1,9 +1,10 @@
 class MyCalendar {
     vector<pair<int, int>> books;
-public:    
+public:
     bool book(int start, int end) {
         for (pair<int, int> p : books)
-            if (max(p.first, start) < min(end, p.second)) return false;
+            if (max(p.first, start) < min(end, p.second))
+                return false;
         books.push_back({start, end});
         return true;
     }
