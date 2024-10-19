e804ac290d596ea9a9d70f0b5a881da123e17df6
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-10-20 00:36:04 +0530

    Time: 14 ms (92.58%) | Memory: 32.1 MB (96.73%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -2,35 +2,44 @@ class Solution {
 public:
     vector<int> findXSum(vector<int>& nums, int k, int x) {
         int n = nums.size();
-        int anslength = n - k + 1;
+        int i = 0;
+        int w = x;
+        int j = k - 1;
+        int sum = 0;
+        unordered_map<int, int> mp;
+        for (int q = i; q <= j; q++) {
+            sum += nums[q];
+            mp[nums[q]]++;
+        }
         vector<int> ans;
-        for (int i = 0; i < n - k + 1; i++) {
-            set<int> st;
-            int sum = 0;
-            unordered_map<int, int> mp;
-            for (int j = i; j < i + k; j++) {
-                sum += nums[j];
-                mp[nums[j]]++;
-                st.insert(nums[j]);
+        priority_queue<pair<int, int>> pq;
+        while (j < n) {
+            for (auto& it : mp) {
+                pq.push({it.second, it.first});
             }
-            if (st.size() < x) {
+            if (mp.size() < x) {
                 ans.push_back(sum);
             } else {
-                vector<pair<int, int>> v;
-                for (auto& it : mp)
-                    v.push_back({it.second, it.first});
-                sort(v.begin(), v.end(),
-                     [&](pair<int, int> a, pair<int, int> b) {
-                         if (a.first == b.first) {
-                             return a.second > b.second;
-                         }
-                         return a.first > b.first;
-                     });
                 int t = 0;
-                for (int q = 0; q < x; q++)
-                    t += v[q].first * v[q].second;
+                while (!pq.empty() && w != 0) {
+                    t += pq.top().first * pq.top().second;
+                    pq.pop();
+                    w--;
+                }
                 ans.push_back(t);
             }
+            mp[nums[i]]--;
+            sum -= nums[i];
+            cout << sum;
+            i++;
+            j++;
+            if (j < n) {
+                sum += nums[j];
+                mp[nums[j]]++;
+            }
+            priority_queue<pair<int, int>> tq;
+            pq = tq;
+            w = x;
         }
         return ans;
     }
