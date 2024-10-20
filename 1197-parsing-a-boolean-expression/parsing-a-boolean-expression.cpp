bb11ffd7212afeff9c6f7e2b0dd002a6292c101a
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-10-20 14:46:59 +0530

    Time: 7 ms (64.14%) | Memory: 12.2 MB (15.28%) - LeetSync

--- 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -1,92 +1,45 @@
 class Solution {
 public:
     bool parseBoolExpr(string expression) {
-        int n = expression.size();
         vector<char> v;
         stack<char> st;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < expression.size(); i++) {
             if (expression[i] == ')') {
                 vector<bool> v1;
                 while (!st.empty() && (st.top() != '&' && st.top() != '|' &&
                                        st.top() != '!')) {
-                    if (st.top() == 'f') {
-                        cout << "hanjii";
+                    if (st.top() == 'f')
                         v1.push_back(false);
-                    }
-                    if (st.top() == 't') {
-                        cout << st.size();
-                        cout << "ttttttttt";
+                    if (st.top() == 't')
                         v1.push_back(true);
-                    }
-                    cout << st.top();
                     st.pop();
                 }
-                cout << "cd";
                 if (!st.empty() &&
-                    (st.top() == '&' || st.top() == '!' || st.top() == '|')) {
+                    (st.top() == '&' || st.top() == '!' || st.top() == '|'))
                     st.pop();
-                }
-                if (v.size() >= 1 && v[v.size() - 1] == '!') {
-                    cout << "hello";
-                    if (v1[0] == false) {
-                        cout << "bh";
-                        st.push('t');
-                    } else {
-
-                        st.push('f');
-                    }
-
-                    v.pop_back();
-                    cout<<v.size();
-                } else {
+                if (v.size() >= 1 && v[v.size() - 1] == '!')
+                    st.push((v1[0] == false) ? 't' : 'f');
+                else {
                     if (v1.size() == 1) {
-                        if (v1[0] == false)
-                            st.push('f');
-                        else
-                            st.push('t');
-                        
-                        v.pop_back();
-
+                        st.push((v1[0] == false) ? 'f' : 't');
                     } else if (v1.size() > 1) {
                         bool x = v1[0];
-
                         char c;
-                        if (v.size() >= 1) {
-                            cout << "jhgcjhvcjhscvjhcvsdjhvcdcj";
+                        if (v.size() >= 1)
                             c = v[v.size() - 1];
-                        }
-                        for (int t = 1; t < v1.size(); t++) {
-
-                            if (c == '|') {
-                                x = x | v1[t];
-                            } else {
-                                cout<<"hhhh";
-                                x = x & v1[t];
-                            }
-                        }
-                        cout << x;
-                        v.pop_back();
-                        cout<<v.size();
-                        if (x == false) {
-                            cout << "hj";
-                            st.push('f');
-                            cout << st.size();
-                        } else {
-                            st.push('t');
-                        }
+                        for (int t = 1; t < v1.size(); t++)
+                            x = (c == '|') ? x | v1[t] : x & v1[t];
+                        st.push((x == false) ? 'f' : 't');
                     }
                 }
+                v.pop_back();
             } else {
                 if (expression[i] == '&' || expression[i] == '!' ||
-                    expression[i] == '|') {
+                    expression[i] == '|')
                     v.push_back(expression[i]);
-                }
                 st.push(expression[i]);
             }
         }
-        if (!st.empty()) {
-            return (st.top() == 'f') ? false : true;
-        }
-        return 0;
+        return (st.top() == 'f') ? false : true;
     }
 };
