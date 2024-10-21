0278ec330cf0332f777b74edde2c9b923717a921
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-10-21 20:30:04 +0530

    Time: 1172 ms (7.89%) | Memory: 405.5 MB (11.99%) - LeetSync

---
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    void solve(unordered_set<string>st,int i,int &maxcount,int currcount,string s)
+    {
+        if(i>=s.length())        
+        {
+            maxcount=max(maxcount,currcount);
+            return;
+        }
+        for(int j=i;j<s.size();j++)
+        {
+            string sub=s.substr(i,j-i+1);
+            if(st.find(sub)==st.end())
+            {
+                st.insert(sub);
+                solve(st,j+1,maxcount,currcount+1,s);
+                st.erase(sub);
+            }
+        }
+    }
+    int maxUniqueSplit(string s) {
+        int maxcount=0;
+        unordered_set<string>st;
+        solve(st,0,maxcount,0,s);
+        return maxcount;
+    }
+};
