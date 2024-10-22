84541e42cd7f96ac6b5a4a2cbfc5964f44f81069
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-10-22 14:26:54 +0530

    Time: 6 ms (98.11%) | Memory: 105.8 MB (54.58%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -1,15 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> v;
@@ -23,27 +11,23 @@ public:
                 TreeNode* root1 = qu.front();
                 qu.pop();
                 sum += root1->val;
-                if (root1->left) {
+                if (root1->left)
                     qu.push(root1->left);
-                }
-                if (root1->right) {
+                if (root1->right)
                     qu.push(root1->right);
-                }
             }
             v.push_back(sum);
         }
     }
     int maxLevelSum(TreeNode* root) {
         levelordertraversal(root);
-        int m = INT_MIN;
-        int ans = 0;
+        int m = INT_MIN, level = 0;
         for (int i = 0; i < v.size(); i++) {
-            cout << v[i];
             if (v[i] > m) {
-                ans = i;
+                level = i + 1;
                 m = v[i];
             }
         }
-        return ans + 1;
+        return level;
     }
 };
