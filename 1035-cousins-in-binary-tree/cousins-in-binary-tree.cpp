004373bcd3b3baa71b0f87ec944abf39630db6b5
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-10-24 01:07:03 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (64.10%) - LeetSync

---
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int q = 0;
+    int q1 = 0;
+    int l = 0;
+    int l1 = 0;
+    void dfs(TreeNode* root, int parent, int x, int y, int lev, int lev2) {
+        if (root == nullptr) {
+            return;
+        }
+        if (root->val == x) {
+            q = parent;
+            l = lev;
+        }
+        if (root->val == y) {
+            q1 = parent;
+            l1 = lev2;
+        }
+        dfs(root->left, root->val, x, y, lev + 1, lev2 + 1);
+        dfs(root->right, root->val, x, y, lev + 1, lev2 + 1);
+    }
+    bool isCousins(TreeNode* root, int x, int y) {
+        dfs(root, -1, x, y, 0, 0);
+        cout << q;
+        cout << q1;
+        if (l != l1) {
+            return false;
+        }
+        if (q == q1) {
+            return false;
+        }
+        return true;
+    }
+};
