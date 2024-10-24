6ae4b6375a0cf232aacff42e95f2324e3f961362
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-10-24 11:53:13 +0530

    Time: 4 ms (23.69%) | Memory: 15.1 MB (6.32%) - LeetSync

---
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool levelorder(TreeNode* root1, TreeNode* root2) {
+        queue<pair<TreeNode*, TreeNode*>> qu;
+        queue<pair<TreeNode*, TreeNode*>> qu1;
+        TreeNode* dummy = new TreeNode(-1);
+        TreeNode* dummy1 = new TreeNode(-1);
+        qu.push({root1, dummy});
+        qu1.push({root2, dummy1});
+        while (!qu.empty() || !qu1.empty()) {
+            int size = qu.size();
+            int size1 = qu1.size();
+            vector<pair<int,int>> v1;
+            vector<pair<int,int>> v2;
+            while (size--) {
+                TreeNode* root = qu.front().first;
+                TreeNode* parent = qu.front().second;
+                qu.pop();
+                if (root->left)
+                    qu.push({root->left, root});
+                if (root->right)
+                    qu.push({root->right, root});
+                v1.push_back({root->val, parent->val});
+                cout<<root-> val;
+                cout<<parent-> val;
+            }
+            while (size1--) {
+                TreeNode* root4 = qu1.front().first;
+                TreeNode* parent = qu1.front().second;
+                qu1.pop();
+                if (root4->left)
+                    qu1.push({root4->left, root4});
+                if (root4->right)
+                    qu1.push({root4->right, root4});
+                v2.push_back({root4->val, parent->val});
+                cout<<root4->val;
+                cout<<parent->val;
+            }
+            if(v1.size()!=v2.size()){
+                return false;
+            }
+            sort(v1.begin(),v1.end());
+            sort(v2.begin(),v2.end());
+            if(v1!=v2)
+            {
+                cout<<"byeee";
+                return false;
+            }
+        }
+        return true;
+    }
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        if(root1!=nullptr && root2!=nullptr)
+        {
+            return levelorder(root1,root2);
+        }
+        if(root1==nullptr && root2==nullptr)
+        {
+            return true;
+        }
+        return false;
+    }
+};
