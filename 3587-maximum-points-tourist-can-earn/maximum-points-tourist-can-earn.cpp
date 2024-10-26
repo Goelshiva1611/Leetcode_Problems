157c82b3357c746e189871dedaaf1eba307922fc
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-10-26 23:44:13 +0530

    Time: 601 ms (78.57%) | Memory: 184.3 MB (7.14%) - LeetSync

--- 3587-maximum-points-tourist-can-earn/maximum-points-tourist-can-earn.cpp
+++ 3587-maximum-points-tourist-can-earn/maximum-points-tourist-can-earn.cpp
@@ -4,31 +4,26 @@ public:
     int x = 0;
     vector<vector<int>> dp;
     int solve(vector<vector<int>>& stayScore, vector<vector<int>>& travelScore,
-              int city, int day) {
-        if (day == t) {
+              int city, int day,int t,int x) {
+        if (day == t) 
             return 0;
-        }
-        if (dp[city][day] != -1) {
+        if (dp[city][day] != -1) 
             return dp[city][day];
-        }
         int stay =
-            stayScore[day][city] + solve(stayScore, travelScore, city, day + 1);
+            stayScore[day][city] + solve(stayScore, travelScore, city, day + 1,t,x);
         int m = 0;
-        for (int j = 0; j < x; j++) {
+        for (int j = 0; j < x; j++) 
                 m = max(m, travelScore[city][j] +
-                               solve(stayScore, travelScore, j, day + 1));
-        }
+                               solve(stayScore, travelScore, j, day + 1,t,x));
         return dp[city][day] = max(stay, m);
     }
     int maxScore(int n, int k, vector<vector<int>>& stayScore,
                  vector<vector<int>>& travelScore) {
-        t = k;
-        x = n;
         dp.resize(201, vector<int>(201, -1));
         int m = 0;
-        for (int i = 0; i < n; i++) {
-            m = max(m, solve(stayScore, travelScore, i, 0));
-        }
+        for (int i = 0; i < n; i++) 
+            m = max(m, solve(stayScore, travelScore, i, 0,k,n));
+        
         return m;
     }
 };
