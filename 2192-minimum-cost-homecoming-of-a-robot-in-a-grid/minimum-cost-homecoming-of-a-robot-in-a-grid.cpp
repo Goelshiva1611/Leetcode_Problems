39783bfe660eb5a21d74a79bef9fcb894ef68088
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-10-27 23:11:05 +0530

    Time: 0 ms (100.00%) | Memory: 152.6 MB (82.86%) - LeetSync

---
+++ 2192-minimum-cost-homecoming-of-a-robot-in-a-grid/minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minCost(vector<int>& startPos, vector<int>& homePos, vector<int>& rowCosts, vector<int>& colCosts) {
+        int ans = 0;
+        if(startPos[0] < homePos[0])
+        {
+            for(int i = startPos[0]+1 ; i <=homePos[0] ; i++)
+            {
+                ans+=rowCosts[i];
+            }
+        }
+        if(startPos[0] > homePos[0])
+        {
+            for(int i = startPos[0]-1 ; i >=homePos[0] ; i--)
+            {
+                ans+=rowCosts[i];
+            }
+        }
+        if(startPos[1] < homePos[1])
+        {
+            for(int i = startPos[1]+1 ; i <=homePos[1] ; i++)
+            {
+                ans+=colCosts[i];
+            }
+        }
+         if(startPos[1] > homePos[1])
+        {
+            for(int i = startPos[1]-1 ; i >=homePos[1] ; i--)
+            {
+                ans+=colCosts[i];
+            }
+        }
+        return ans;
+    }
+};
