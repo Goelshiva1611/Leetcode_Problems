d513f0e6b57f37a2e85377835db25bb965d345a0
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-10-29 01:09:57 +0530

    Time: 54 ms (12.73%) | Memory: 101.3 MB (5.37%) - LeetSync

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
+    int n = 0;
     int mod = 1e9 + 7;
-    int n;
     vector<vector<long long>> dp;
-    
     int solve(int j, int stp) {
-        if (j < 0 || j >= n) return 0;
-        if (stp == 0) return j == 0 ? 1 : 0;
+        if (j < 0 || j >= n)
+            return 0;
 
-        if (dp[j][stp] != -1) return dp[j][stp];
+        if (stp == 0) {
+            if (j == 0)
+                return 1;
+            else
+                return 0;
+        }
 
-        long long left = (j > 0) ? solve(j - 1, stp - 1) : 0;
-        long long right = (j < n - 1) ? solve(j + 1, stp - 1) : 0;
-        long long stay = solve(j, stp - 1);
+        if (dp[j][stp] != -1)
+            return dp[j][stp];
 
+        long long left = 0;
+        long long right = 0;
+        stp -= 1;
+        if (j > 0)
+            left = solve(j - 1, stp);
+        if (j < n - 1)
+            right = solve(j + 1, stp);
+
+        long long stay = solve(j, stp);
+        stp+=1;
         return dp[j][stp] = (left + right + stay) % mod;
     }
-    
     int numWays(int steps, int arrLen) {
-        n = min(arrLen, steps / 2 + 1); // optimize bounds
-        dp.resize(n, vector<long long>(steps + 1, -1));
+        n = arrLen;
+        dp.resize(501, vector<long long>(501, -1));
         return solve(0, steps);
     }
 };
