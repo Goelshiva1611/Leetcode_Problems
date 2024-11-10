b62f3bb29094b9035421f1559e143721a148e33d
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-11-10 21:25:13 +0530

    Time: 285 ms (25.27%) | Memory: 273.9 MB (7.80%) - LeetSync

---
+++ 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void addthisinvector(vector<int>& v, int num) {
+        for (int i = 0; i < 32; i++) {
+            if ((num & (1 << i)) != 0) {
+                v[i]++;
+            }
+        }
+    }
+    void removethisinvector(vector<int>& v, int num) {
+        for (int i = 0; i < 32; i++) {
+            if ((num & (1 << i)) != 0) {
+                v[i]--;
+            }
+        }
+    }
+    int decimalnumber(vector<int> v) {
+        int num = 0;
+        for (int i = 0; i < 32; i++) {
+            if (v[i] > 0)
+                num = num | (1 << i);
+        }
+        return num;
+    }
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        int i = 0;
+        int j = 0;
+        int n = nums.size();
+        int result = INT_MAX;
+        vector<int> v(32, 0);
+        while (j < n) {
+            addthisinvector(v, nums[j]);
+            while (i <= j && decimalnumber(v) >= k) {
+                result = min(result, j - i + 1);
+                removethisinvector(v, nums[i]);
+                i++;
+            }
+            j++;
+        }
+        return (result == INT_MAX) ? -1 : result;
+    }
+};
