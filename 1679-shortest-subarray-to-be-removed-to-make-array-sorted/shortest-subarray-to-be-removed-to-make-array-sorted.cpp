29f289cdc41206b76032dd4208d0c0560d9d23a2
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-11-15 12:07:11 +0530

    Time: 2641 ms (5.14%) | Memory: 69.7 MB (25.81%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -16,32 +16,27 @@ public:
                 break;
             }
         }
-        int m=INT_MAX;
-        for(int i=p;i>=0;i--){
-            for(int j=n-1;j>=q;j--)
-            {
-                if(arr[i]>arr[j])
-                {
-                    m=min(m,j-i);
-                    cout<<m;
+        int m = INT_MAX;
+        for (int i = p; i >= 0; i--) {
+            for (int j = n - 1; j >= q; j--) {
+                if (arr[i] > arr[j]) {
+                    m = min(m, j - i);
+                    cout << m;
                     break;
                 }
             }
         }
-        for(int i=q;i<n;i++){
-            for(int j=0;j<=p;j++)
-            {
-                if(arr[j]>arr[i])
-                {
-                    m=min(m,i-j);
-                    cout<<m;
+        for (int i = q; i < n; i++) {
+            for (int j = 0; j <= p; j++) {
+                if (arr[j] > arr[i]) {
+                    m = min(m, i - j);
+                    cout << m;
                     break;
                 }
             }
         }
-        if(m==INT_MAX)
-        {
-            return (q-p-1 < 0)? 0 :q-p-1 ;
+        if (m == INT_MAX) {
+            return (q - p - 1 < 0) ? 0 : q - p - 1;
         }
 
         return m;
