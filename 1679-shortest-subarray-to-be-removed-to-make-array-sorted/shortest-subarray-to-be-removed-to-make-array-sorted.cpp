46507f3c9591c93fca746558717b34235814d037
Shivansh Goel <shivanshgoel1611@gmail.com>
2024-11-15 12:00:23 +0530

    Time: 2635 ms (5.14%) | Memory: 69.6 MB (25.81%) - LeetSync

---
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int findLengthOfShortestSubarray(vector<int>& arr) {
+        int n = arr.size();
+        int p = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i + 1] < arr[i]) {
+                p = i;
+                break;
+            }
+        }
+        int q = 0;
+        for (int i = n - 1; i >= 1; i--) {
+            if (arr[i - 1] > arr[i]) {
+                q = i;
+                break;
+            }
+        }
+        int m=INT_MAX;
+        for(int i=p;i>=0;i--){
+            for(int j=n-1;j>=q;j--)
+            {
+                if(arr[i]>arr[j])
+                {
+                    m=min(m,j-i);
+                    cout<<m;
+                    break;
+                }
+            }
+        }
+        for(int i=q;i<n;i++){
+            for(int j=0;j<=p;j++)
+            {
+                if(arr[j]>arr[i])
+                {
+                    m=min(m,i-j);
+                    cout<<m;
+                    break;
+                }
+            }
+        }
+        if(m==INT_MAX)
+        {
+            return (q-p-1 < 0)? 0 :q-p-1 ;
+        }
+
+        return m;
+    }
+};
