95efc6b30e23897b80fc66bd7c888f1781b46397
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-01-12 15:20:01 +0530

    Time: 15 ms (51.52%) | Memory: 30.2 MB (53.05%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,34 +1,63 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
-        if (s.size() % 2 != 0) {
+        // if (s.size() % 2 != 0) {
+        //     return false;
+        // }
+        // stack<int> open;
+        // stack<int> openclose;
+        // int n = s.size();
+        // for (int i = 0; i < n; i++) {
+        //     if (locked[i] == '0') {
+        //         openclose.push(i);
+        //     } else {
+        //         if (s[i] == '(') {
+        //             open.push(i);
+        //         } else {
+        //             if (!open.empty()) {
+        //                 open.pop();
+        //             } else if (!openclose.empty()) {
+        //                 openclose.pop();
+        //             } else {
+        //                 return false;
+        //             }
+        //         }
+        //     }
+        // }
+        // while (!open.empty() && !openclose.empty() &&
+        //        open.top() < openclose.top()) {
+        //     open.pop();
+        //     openclose.pop();
+        // }
+        // return open.empty();
+
+        int open = 0;
+        int close = 0;
+        int n = s.size();
+        if(n%2!=0)
+        {
             return false;
         }
-        stack<int> open;
-        stack<int> openclose;
-        int n = s.size();
         for (int i = 0; i < n; i++) {
-            if (locked[i] == '0') {
-                openclose.push(i);
+            if (locked[i] == '0' || s[i] == '(') {
+                open++;
             } else {
-                if (s[i] == '(') {
-                    open.push(i);
-                } else {
-                    if (!open.empty()) {
-                        open.pop();
-                    } else if (!openclose.empty()) {
-                        openclose.pop();
-                    } else {
-                        return false;
-                    }
+                open--;
+                if (open < 0) {
+                    return false;
                 }
             }
         }
-        while (!open.empty() && !openclose.empty() &&
-               open.top() < openclose.top()) {
-            open.pop();
-            openclose.pop();
+        for (int i = n-1; i >=0; i--) {
+            if (locked[i] == '0' || s[i] == ')') {
+                close++;
+            } else {
+                close--;
+                if (close < 0) {
+                    return false;
+                }
+            }
         }
-        return open.empty();
+        return true;
     }
 };
