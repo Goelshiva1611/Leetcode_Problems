1f0cfcc96d2665e99f36d39568bff8d964ae097f
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-01-14 18:05:32 +0530

    Time: 4 ms (69.55%) | Memory: 85.7 MB (85.75%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,37 +1,12 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        int n = A.size();
-        set<int> st;
+        int t=0;
         vector<int> c;
         vector<bool> visited(A.size(), false);
-        // for (int i = 0; i < n; i++) {
-        //     int count = 0;
-        //     st.insert(A[i]);
-        //     for (int j = 0; j <= i; j++) {
-        //         if (st.find(B[j]) != st.end()) {
-        //             count++;
-        //         }
-        //     }
-        //     c.push_back(count);
-        // }
-        int t = 0;
-        for (int i = 0; i < n; i++) {
-            int a = A[i];
-            int b = B[i];
-
-            if (visited[a] == false) {
-                visited[a] = true;
-            } else {
-                t++;
-            }
-
-            if (visited[b] == false) {
-                visited[b] = true;
-            } else {
-                t++;
-            }
-
+        for (int i = 0; i < A.size(); i++) {
+            visited[A[i]] == false ? visited[A[i]] = true : t++;
+            visited[B[i]] == false ? visited[B[i]] = true : t++;
             c.push_back(t);
         }
         return c;
