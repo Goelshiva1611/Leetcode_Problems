3b63f1ab2ef5e1876c83b416c8008e729ac08db0
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-01-28 15:09:43 +0530

    Time: 57 ms (8.23%) | Memory: 96.8 MB (44.66%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
-    int n;
-    int m;
-    void dfs(int i, int j, vector<vector<int>>& grid,
-             vector<vector<int>>& visited, int& sum) {
-        if (i < 0 || i >= n || j >= m || j < 0) {
+    void dfs(vector<vector<int>>& grid, int i, int j,
+             vector<vector<bool>>&visited, int& sum) {
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() ||
+            visited[i][j] == true || grid[i][j] == 0) {
             return;
         }
-        if (visited[i][j] == 1 || grid[i][j] == 0) {
-            return;
-        }
-        visited[i][j] = 1;
+        visited[i][j] = true;
         sum += grid[i][j];
-        dfs(i + 1, j, grid, visited, sum);
-        dfs(i, j + 1, grid, visited, sum);
-        dfs(i - 1, j, grid, visited, sum);
-        dfs(i, j - 1, grid, visited, sum);
+        cout<<sum;
+        dfs(grid, i + 1, j, visited, sum);
+        dfs(grid, i, j + 1, visited, sum);
+        dfs(grid, i, j - 1, visited, sum);
+        dfs(grid, i - 1, j, visited, sum);
     }
     int findMaxFish(vector<vector<int>>& grid) {
-        n = grid.size();
-        m = grid[0].size();
-        vector<int> v;
-        vector<vector<int>> visited(n, vector<int>(m, 0));
+        int n = grid.size();
+        int ans = 0;
+        int m = grid[0].size();
+        vector<vector<bool>> visited;
+        visited.resize(n, vector<bool>(m, false));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (visited[i][j] == 0 && grid[i][j] >= 1) {
+                if (!visited[i][j] && grid[i][j] != 0) {
                     int sum = 0;
-                    dfs(i, j, grid, visited, sum);
-                    v.push_back(sum);
+                    dfs(grid, i, j, visited, sum);
+                    ans = max(ans, sum);
                 }
             }
         }
-        sort(v.rbegin(), v.rend());
-        if(v.size()==0)
-        {
-            return 0;
-        }
-        return v[0];
+        return ans;
     }
 };
