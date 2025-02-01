f9504085101e485a743fe607f40431b16d58a768
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-02-01 17:48:50 +0530

    Time: 4 ms (74.68%) | Memory: 126.6 MB (79.92%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
+    //very precise and clean solution after doing much more formatting and using gernary operator;
     vector<bool> isArraySpecial(vector<int>& nums,
                                 vector<vector<int>>& queries) {
-        vector<pair<bool, pair<int, int>>> v;
-        int n = nums.size();
-        int q = queries.size();
-        v.resize(n,{false,{0,0}});
-
+        vector<int> v(nums.size(), 0);
         vector<bool> ans;
-        v[0] = {true, {1, 0}};
-        for (int i = 1; i < n; i++) {
-            if ((nums[i] + nums[i - 1]) % 2 == 0) // same parity
-            {
-                v[i] = {false,
-                        {v[i - 1].second.first, v[i - 1].second.second + 1}};
-            } else {
-                v[i] = {true,
-                        {v[i - 1].second.first + 1, v[i - 1].second.second}};
-            }
-            cout<<v[i].second.first;
-            cout<<v[i].second.second;
-            cout<<endl;
+        for (int i = 1; i < nums.size(); i++) {
+            v[i] = ((nums[i] + nums[i - 1]) % 2 == 0) ? v[i - 1] + 1 : v[i - 1];
         }
-
-        for (int i = 0; i < q; i++) {
-            // now first query
-            int a = queries[i][0]; // start
-            int b = queries[i][1]; // end
-            int diff = v[b].second.second - v[a].second.second;
-            if (diff > 0) {
-                ans.push_back(false);
-            } else {
-                ans.push_back(true);
-            }
+        for (int i = 0; i < queries.size(); i++) {
+            ans.push_back((v[queries[i][1]] - v[queries[i][0]]) > 0 ? false
+                                                                    : true);
         }
         return ans;
     }
