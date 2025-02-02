6b9573e6891298d904cea07c026b0c61c5d0fafc
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-02-02 11:34:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (5.21%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,31 +1,6 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        vector<int> p;
-        vector<int> t;
-        vector<int> s;
-        s = nums;
-        sort(s.begin(), s.end());
-        int x = 0;
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] - nums[i - 1] < 0) {
-                p.push_back(nums[i - 1]);
-                x = i;
-                break;
-            }
-            p.push_back(nums[i - 1]);
-        }
-        for (int i = x; i < nums.size(); i++)
-            t.push_back(nums[i]);
-        if (x != 0) {
-            for (int i = 0; i < p.size(); i++)
-                t.push_back(p[i]);
-        }
-        return (t == s) ? true : false;
-    }
-};
-
-/* bool check(vector<int>& nums) {
         vector<int> x = nums;
         sort(x.begin(), x.end());
         vector<int> p(2 * x.size(), 0);
@@ -52,4 +27,48 @@ public:
                 qu.push(p[l]);
         }
         return false;
+    }
+};
+
+/* bool check(vector<int>& nums) {
+        int dec = 0;
+        vector<int> sorted = nums;
+        sort(sorted.begin(), sorted.end());
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] - nums[i - 1] < 0) {
+                dec++;
+                if (dec > 1 || (nums[i]) != sorted[0] ||
+                    nums[i - 1] != sorted[sorted.size() - 1])
+                    return false;
+            }
+        }
+        return true;
     }*/
+/* */
+
+/*
+bool check(vector<int>& nums) {
+    vector<int> p; // p left of nums
+    vector<int> t; // t right of nums till decreasing
+    vector<int> s; // sorted
+    s = nums;
+    // right+left==sortes then true;
+    sort(s.begin(), s.end());
+    int x = 0;
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] - nums[i - 1] < 0) {
+            p.push_back(nums[i - 1]);
+            x = i;
+            break;
+        }
+        p.push_back(nums[i - 1]);
+    }
+    for (int i = x; i < nums.size(); i++)
+        t.push_back(nums[i]);
+    if (x != 0) {
+        for (int i = 0; i < p.size(); i++)
+            t.push_back(p[i]);
+    }
+    return (t == s) ? true : false;
+}
+*/
