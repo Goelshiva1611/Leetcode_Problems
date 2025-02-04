e7d915e2821f0462881304515ac7e0218b61ec3d
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-02-04 22:53:27 +0530

    Time: 0 ms (100.00%) | Memory: 69.9 MB (31.67%) - LeetSync

--- 2145-grid-game/grid-game.cpp
+++ 2145-grid-game/grid-game.cpp
@@ -3,8 +3,8 @@ public:
     typedef long long l;
     long long gridGame(vector<vector<int>>& grid) {
         l n = grid.size(), m = grid[0].size(), fr = 0, sum = 0;
-        for (int i = 0; i < m; i++)
-            fr += grid[0][i];
+        vector<int> v = grid[0];
+        fr = accumulate(v.begin(), v.end(), 0l);
         fr -= grid[0][0];
         l robot2 = fr;
         for (int i = 1; i < m; i++) {
