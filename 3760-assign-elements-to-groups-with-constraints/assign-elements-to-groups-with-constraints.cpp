e1f126d474b05151b0cc83a802794194c674f4af
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-02-11 21:43:38 +0530

    Time: 1550 ms (12.62%) | Memory: 254.7 MB (48.56%) - LeetSync

--- 3760-assign-elements-to-groups-with-constraints/assign-elements-to-groups-with-constraints.cpp
+++ 3760-assign-elements-to-groups-with-constraints/assign-elements-to-groups-with-constraints.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     vector<int> assignElements(vector<int>& g, vector<int>& e) {
-        int n = g.size() ;
-        int m = e.size() ;
-        map<int , int> map ;
-        for(int i = 0 ; i < m ; i++) {
-            if( map.find(e[i]) == map.end() )
-             map[e[i]] = i ;
+        int n = g.size();
+        int m = e.size();
+        map<int, int> map;
+        for (int i = 0; i < m; i++) {
+            if (map.find(e[i]) == map.end())
+                map[e[i]] = i;
         }
-
-        vector<int> v ;
-
-        for(int i = 0 ; i < n ; i++) {
-            int ans = INT_MAX ;
-            for(int j = 1 ; j*j<=(g[i]) ;j++) {
-                if( g[i] % j == 0 && map.find(j) != map.end()) {
-                    ans = min(map[j] , ans) ;
-                } 
-
-                int div = g[i] / j ;
-                if( g[i] % div == 0 && map.find(div) != map.end()) {
-                    ans = min(map[div] , ans) ;
-                } 
+        vector<int> v;
+        for (int i = 0; i < n; i++) {
+            int ans = INT_MAX;
+            for (int j = 1; j * j <= g[i]; j++) {
+                if (g[i] % j == 0) {
+                    if (map.find(j) != map.end())
+                        ans = min(ans, map[j]);
+                    if (j != g[i] / j && map.find(g[i] / j) != map.end())
+                        ans = min(ans, map[g[i] / j]);
+                }
             }
-
-            if( ans == INT_MAX)  ans = -1 ;
-            v.push_back(ans) ;
+            if (ans == INT_MAX)
+                ans = -1;
+            v.push_back(ans);
         }
 
-        return v ;
-
+        return v;
     }
-        
 };
+
+/*
+for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            divisors.push_back(i);
+            if (i != n / i)  // Avoid duplicate when n is a perfect square
+                divisors.push_back(n / i);
+        }
+    }
+*/
