0458dcf19cecb2a3a5226c74a77ae01984e7c4ed
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-02-14 11:05:19 +0530

    Time: 20 ms (68.21%) | Memory: 79.1 MB (5.54%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
@@ -1,26 +1,59 @@
 class ProductOfNumbers {
 public:
-    vector<int>v;
-    ProductOfNumbers() {
-        
-    }
-    
+    typedef long long int ll;
+    vector<ll> v;
+    vector<ll> ans;
+    ll sum = 1;
+    ll x = -1;
+    ProductOfNumbers() {}
     void add(int num) {
-        v.push_back(num);
+        if (num == 0) {
+            x = ans.size();
+        }
+        sum *= num;
+        if (sum == 0) {
+            sum = 1;
+            ans.push_back(1);
+        } else
+            ans.push_back(sum);
     }
-    
+
     int getProduct(int k) {
-        int sum=1;
-        int  n=v.size();
-        for(int i=n-1;i>=n-k;i--)
-        {
-            sum*=v[i];
+        ll n = ans.size();
+        if (n - k <= x)
+            return 0;
+        if (k != ans.size()) {
+            ll a = sum;
+            ll b = ans[ans.size() - k - 1];
+            return a / b;
         }
         return sum;
-        
     }
 };
 
+
+/*
+class ProductOfNumbers {
+public:
+    vector<int> v;
+    ProductOfNumbers() {}
+    void add(int num) { v.push_back(num); }
+    int getProduct(int k) {
+        int sum = 1;
+        int n = v.size();
+        for (int i = n - 1; i >= n - k; i--) {
+            sum *= v[i];
+        }
+        return sum;
+    }
+};
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
 /**
  * Your ProductOfNumbers object will be instantiated and called as such:
  * ProductOfNumbers* obj = new ProductOfNumbers();
