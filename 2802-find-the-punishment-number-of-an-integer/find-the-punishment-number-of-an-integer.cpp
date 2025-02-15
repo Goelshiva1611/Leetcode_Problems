e7a668190f21549416da8a1a667f93167529e12b
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-02-15 12:39:13 +0530

    Time: 18 ms (76.32%) | Memory: 8 MB (82.37%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
-    bool check(int i, int currsum, string s, int num, vector<vector<int>>& t) {
-        if (i == s.length()) {
-            return currsum == num;
+    bool check(int num, int currnum, int target) {
+        if(num == 0) {
+            return currnum == target;
         }
-        if (currsum > num)
-            return false;
-
-        if (t[i][currsum] != -1)
-            return t[i][currsum];
-
-        for (int j = i; j < s.length(); j++) {
-            string currentString = s.substr(i, j - i + 1);
-            int addend = stoi(currentString);
-            if (check(j + 1, currsum + addend, s, num, t) == true) {
-                return true;
-            }
-        }
-        return t[i][currsum] = false;
+        
+        return check(num / 10, currnum + num % 10, target) ||
+               check(num / 100, currnum + num % 100, target) ||
+               check(num / 1000, currnum + num % 1000, target) ||
+               check(num / 10000, currnum + num % 10000, target);
     }
 
     int punishmentNumber(int n) {
         int punishmentNum = 0;
-        for (int num = 1; num <= n; num++) {
-            int squareNum = num * num;
-            string s = to_string(squareNum);
-
-            vector<vector<int>> t(s.size(), vector<int>(num + 1, -1));
-
-            if (check(0, 0, s, num, t)) {
+        for (int currentNum = 1; currentNum <= n; currentNum++) {
+            int squareNum = currentNum * currentNum;
+            if (check(squareNum, 0, currentNum)) {
                 punishmentNum += squareNum;
             }
         }
         return punishmentNum;
     }
 };
+
