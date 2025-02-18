ad432ffd125186b654c7ed8a4ddcf5370719ad63
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-02-18 16:49:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (18.66%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,40 +1,48 @@
 class Solution {
 public:
-    set<string> result;
-    vector<int> v;
-
-    void solve(string p, string ans, int i) {
-        if (i == p.size()) {
-            cout << ans;
-            result.insert(ans);
-            return;
-        }
-        for (int j = 1; j < 10; j++) {
-            char newchar = p[i];
-            if (v[j] == 1)
+    string smallestNumber(string s) {
+        int n = s.size();
+        queue<pair<int, int>> qu;
+        for (int i = 0; i < n;) {
+            if (s[i] == 'D') {
+                int j = i;
+                while (i < n && s[i] != 'I') {
+                    i++;
+                }
+                qu.push({j, i - j});
                 continue;
-            if ((newchar == 'I' && j > (ans[ans.size() - 1] - '0')) ||
-                (newchar == 'D' && (ans[ans.size() - 1] - '0') > j)) {
-                v[j] = 1;
-                ans += to_string(j);
-                solve(p, ans, i + 1);
-                v[j] = 0;
-                ans.pop_back();
-            } 
+            }
+            i++;
         }
-    }
-    string smallestNumber(string pattern) {
-
-        v.resize(10, 0);
-        int n = pattern.size();
-        for (int i = 1; i <= 9; i++) {
-            string ans = "";
-            v[i] = 1;
-            ans += to_string(i);
-            cout << ans;
-            solve(pattern, ans, 0);
-            v[i] = 0;
+        int p = 1;
+        string ans = "";
+        int x=0;
+        while (!qu.empty()) {
+            int a = qu.front().first;
+            int b = qu.front().second;
+            for (int i = ans.size(); i < a; i++) {
+                ans += to_string(p);
+                p++;
+            }
+            p+=b;
+            x=p;
+            for(int i=a;i<=a+b;i++)
+            {
+                ans += to_string(x);
+                x--;             
+            }
+            qu.pop();
+            p++;
         }
-        return *result.begin();
+        if(ans.size()==n+1)
+        {
+            return ans;
+        }
+        while(ans.size()<=n)
+        {
+            ans+=to_string(p);
+            p++;
+        }
+        return ans;
     }
 };
