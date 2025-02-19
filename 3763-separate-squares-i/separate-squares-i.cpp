63ec8d804835803c7c80533fc8838fe405002321
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-02-19 18:14:28 +0530

    Time: 211 ms (17.65%) | Memory: 198.8 MB (18.00%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.cpp
+++ 3763-separate-squares-i/separate-squares-i.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
     pair<double, double> getArea(vector<vector<int>>& squares, double mid) {
-    double upper = 0, lower = 0;
-    for (auto& s : squares) {
-        if (s[1] + s[2] <= mid)
-            lower += (double)s[2] * (double)s[2];
-        else if (s[1] >= mid)
-            upper += (double)s[2] * (double)s[2];
-        else {
-            lower += (mid - (double)s[1]) * (double)s[2];
-            upper += ((double)s[1] + (double)s[2] - mid) * (double)s[2];
+        double upper = 0, lower = 0;
+        for (auto& s : squares) {
+            if (s[1] + s[2] <= mid)
+                lower += (double)s[2] * (double)s[2];
+            else if (s[1] >= mid)
+                upper += (double)s[2] * (double)s[2];
+            else {
+                lower += (mid - (double)s[1]) * (double)s[2];
+                upper += ((double)s[1] + (double)s[2] - mid) * (double)s[2];
+            }
         }
-    }
-    return {lower, upper};
-}
-
-double separateSquares(vector<vector<int>>& squares) {
-    double low = 0, high = INT_MAX, mid = 0, ans = 0, possibleMid = 0;
-    while (low <= high) {
-        mid = low + (high - low) / 2;
-        auto area = getArea(squares, mid);
-        if (ans == mid)
-            break;
-        if (area.first > area.second)
-            high = mid;
-        else
-            low = mid;
-        ans = mid;
+        return {lower, upper};
     }
 
-    // check further, if the mid is not overlapping with any square then return
-    // answer as the larget value of upper y value of square which is smaller
-    // then mid.
-    for (auto s : squares) {
-        if (ans >= (double)(s[1] + s[2]))
-            possibleMid = max(possibleMid, (double)(s[1] + s[2]));
-        if (ans > (double)s[1] && ans < (double)(s[1] + s[2]))
-            possibleMid = INT_MAX;
+    double separateSquares(vector<vector<int>>& squares) {
+        double low = 0, high = 2e9, mid = 0, ans = 0, possibleMid = 0;
+        sort(squares.begin(), squares.end());
+        while (high - low >1e-5) {
+            mid = low + (high - low) / 2;
+            auto area = getArea(squares, mid);
+            if (area.first < area.second)
+                low = mid;
+            else
+                high = mid;
+        }
+        return round(high*1e5)/1e5;
     }
-    return min(possibleMid, ans);
-}
 };
