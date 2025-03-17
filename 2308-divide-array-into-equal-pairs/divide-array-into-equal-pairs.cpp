eb23a7bbc48b3d10d86ff6583bf04715a54271c9
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-03-17 15:48:20 +0530

    Time: 7 ms (19.84%) | Memory: 18.1 MB (49.04%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -4,6 +4,8 @@ public:
         unordered_map<int, int> mp;
         for (int i = 0; i < nums.size(); i++)
             mp[nums[i]]++;
+        if(mp.size()>nums.size()/2)
+            return false;
         for (auto it : mp)
             if (it.second % 2 != 0)
                 return false;
