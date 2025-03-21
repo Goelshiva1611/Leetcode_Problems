992f39ccd4fdc5142b738438e58077631c0bc261
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-03-21 14:18:01 +0530

    Time: 84 ms (73.29%) | Memory: 107.8 MB (74.68%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
-    vector<string> findAllRecipes(vector<string>& rec, vector<vector<string>>& ing, vector<string>& sup) {
-        unordered_map<string,vector<string>> graph;
-        int n = rec.size();
-        unordered_set<string> s;
-        for(auto x : sup) s.insert(x);            //store all the supplies in unordered set
-		
-        unordered_map<string,int> indegree;   //to store the indegree of all recipes
-        for(auto x : rec)indegree[x] = 0;                      //initially take the indegree of all recipes to be 0
-    
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < (int)ing[i].size(); j++){
-                if(s.find(ing[i][j]) == s.end()){     
-                    graph[ing[i][j]].push_back(rec[i]);    //if the ingredient required to make a recipe is not in supplies then  
-                    indegree[rec[i]]++;                     //we need to make a directed edge from that ingredient to recipe
+    vector<string> findAllRecipes(vector<string>& r,
+                                  vector<vector<string>>& ing,
+                                  vector<string>& sup) {
+        unordered_map<string, vector<string>> mp;
+        unordered_map<string, int> indegree;
+        set<string> st;
+        for (int i = 0; i < sup.size(); i++) {
+            st.insert(sup[i]);
+        }
+        for (int i = 0; i < r.size(); i++) {
+            indegree[r[i]] = 0;
+        }
+        for (int i = 0; i < r.size(); i++) {
+            for (int j = 0; j < ing[i].size(); j++) {
+                if (st.find(ing[i][j]) == st.end()) {
+                    indegree[r[i]]++;
+                    mp[ing[i][j]].push_back(r[i]);
                 }
             }
         }
-        
-        //KAHN'S ALGORITHM
-        queue<string> q;
-        for(auto x : indegree){
-            if(x.second == 0){
-                q.push(x.first);
+        queue<string> qu;
+        for (auto it : indegree) {
+            if (it.second == 0)
+                qu.push(it.first);
+        }
+        vector<string> answer;
+        while (!qu.empty()) {
+            string p = qu.front();
+            qu.pop();
+            answer.push_back(p);
+            for (int i = 0; i < mp[p].size(); i++) {
+                indegree[mp[p][i]]--;
+                if (indegree[mp[p][i]] == 0) {
+                    qu.push(mp[p][i]);
+                }
             }
         }
-       vector<string> ans;
-        while(!q.empty()){
-            string tmp = q.front();
-            q.pop();
-            ans.push_back(tmp);
-            for(auto nbr : graph[tmp]){
-                indegree[nbr]--;
-                if(indegree[nbr] == 0)
-                    q.push(nbr);
-            }
-        }
-        return ans;
+        return answer;
     }
 };
