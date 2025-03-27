b8133985ef0ba1a0ea6d596bb6ad1dbf9494d747
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-03-27 23:09:02 +0530

    Time: 103 ms (7.60%) | Memory: 123.2 MB (5.26%) - LeetSync

---
+++ 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    priority_queue<int> maxpq;
+
+    int partitionDisjoint(vector<int>& nums) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            minpq;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            minpq.push({nums[i], i});
+        }
+        for (int i = 0; i < n; i++) {
+            maxpq.push(nums[i]);
+            while (!minpq.empty() && minpq.top().second <= i) {
+                minpq.pop();
+            }
+            if (!minpq.empty() && minpq.top().first < maxpq.top()) {
+                if (minpq.top().first == nums[i] && minpq.top().second == i)
+                    minpq.pop();
+            } else {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+};
