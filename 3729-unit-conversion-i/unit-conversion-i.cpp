e7de8ecaa8ef6a163cc056174d53ab22c32c21b8
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-04-27 00:47:05 +0530

    Time: 8 ms (93.55%) | Memory: 201.5 MB (95.71%) - LeetSync

--- 3729-unit-conversion-i/unit-conversion-i.cpp
+++ 3729-unit-conversion-i/unit-conversion-i.cpp
@@ -3,15 +3,17 @@ public:
     vector<int> baseUnitConversions(vector<vector<int>>& conversions) {
         int n = conversions.size();
         vector<int> answer;
-        int modulo=1000000007;
-        vector<int  > units(n+1, 1);
+        int modulo = 1000000007;
+        vector<int> units(n + 1, 1);
         answer.push_back(1);
         for (int i = 0; i < n; i++) {
-            int a = conversions[i][0]%modulo;
-            int b = conversions[i][1]%modulo;
-            int c = conversions[i][2]%modulo;
-            int t=((long long)(c%modulo)*(units[a]%modulo))%modulo;
-            units[b]=t;
+            int a = conversions[i][0] % modulo;
+            int b = conversions[i][1] % modulo;
+            int c = conversions[i][2] % modulo;
+            long long p=1;
+            long long  t = ( p*c * units[a]) % modulo;
+            //1e9 is double * double throws a error so you have a need to typecast this one 
+            units[b] = t;
         }
         return units;
     }
