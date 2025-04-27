5d16519cbe331b9d91f1e9a2b71daf1adc466268
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-04-27 19:08:36 +0530

    Time: 243 ms (90.82%) | Memory: 347 MB (87.79%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.cpp
+++ 3819-count-covered-buildings/count-covered-buildings.cpp
@@ -6,39 +6,18 @@ public:
         for (int i = 0; i < buildings.size(); i++) {
             int a = buildings[i][0];
             int b = buildings[i][1];
-            if (columns[b][0] == 0) {
-                cout << a;
-                columns[b][0] = a;
-            } else {
-                columns[b][0] = min(columns[b][0], a);
-            }
-
-            if (columns[b][1] == 0) {
-                columns[b][1] = a;
-            } else {
-                columns[b][1] = max(columns[b][1], a);
-            }
-
-            if (rows[a][0] == 0) {
-                rows[a][0] = b;
-            } else {
-                rows[a][0] = min(rows[a][0], b);
-            }
-
-            if (rows[a][1] == 0) {
-                rows[a][1] = b;
-            } else {
-                rows[a][1] = max(rows[a][1], b);
-            }
+            columns[b][0] = (columns[b][0] == 0) ? a : min(columns[b][0], a);
+            columns[b][1] = (columns[b][1] == 0) ? a : max(columns[b][1], a);
+            rows[a][0] = (rows[a][0] == 0) ? b : min(rows[a][0], b);
+            rows[a][1] = (rows[a][1] == 0) ? b : max(rows[a][1], b);
         }
         int answer = 0;
         for (int i = 0; i < buildings.size(); i++) {
             int a = buildings[i][0];
             int b = buildings[i][1];
             if ((rows[a][0] < b && rows[a][1] > b) &&
-                (columns[b][0] < a && columns[b][1] > a)) {
+                (columns[b][0] < a && columns[b][1] > a))
                 answer++;
-            }
         }
         return answer;
     }
