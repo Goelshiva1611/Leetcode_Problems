252daa4e196bfa9553bdc46a101ff346b2cfb210
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-05-03 12:39:54 +0530

    Time: 15 ms (13.79%) | Memory: 115.4 MB (76.51%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int n = tops.size();
-        int m = bottoms.size();
+        int p = 0, count = 0, count1 = 0, n = tops.size();
         unordered_map<int, int> mp;
         for (int i = 0; i < n; i++) {
             mp[tops[i]]++;
-        }
-        for (int i = 0; i < m; i++) {
             mp[bottoms[i]]++;
         }
-        int p = 0;
-        for (auto it : mp) {
-            if (it.second >= n) {
+        for (auto it : mp)
+            if (it.second >= n)
                 p = it.first;
-            }
-        }
-        int count = 0;
         for (int i = 0; i < n; i++) {
             if (tops[i] != p) {
-                if (bottoms[i] != p) {
+                if (bottoms[i] != p)
                     return -1;
-                } else {
+                else
                     count++;
-                }
             }
-        }
-        int count1 = 0;
-        for (int i = 0; i < n; i++) {
             if (bottoms[i] != p) {
-                if (tops[i] != p) {
+                if (tops[i] != p)
                     return -1;
-                } else {
+                else
                     count1++;
-                }
             }
         }
         return min(count, count1);
