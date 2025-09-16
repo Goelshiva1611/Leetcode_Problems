5026e9faf435dc2ed303a6bb63a54d3a501d56b0
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-09-16 17:10:35 +0530

    Time: 6 ms (96.62%) | Memory: 59.8 MB (84.54%) - LeetSync

---
+++ 3999-minimum-operations-to-transform-string/minimum-operations-to-transform-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int ans = INT_MIN;
+        for (auto it : s) {
+            char a = it;
+            int p = 26 - (a - 'a');
+            if (p != 26)
+                ans = max(ans, p);
+            else
+                ans = max(ans, 0);
+        }
+        return ans;
+    }
+};
