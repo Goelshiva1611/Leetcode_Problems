57022065df0dce34fe539ec0b0414923d816c95d
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-09-18 01:39:45 +0530

    Time: 157 ms (40.72%) | Memory: 174 MB (55.13%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
@@ -1,25 +1,24 @@
 class FoodRatings {
 public:
-    unordered_map<string, set<pair<int, string>>> cuisine_ratings;
-    unordered_map<string, string> food_cuisine;
-    unordered_map<string, int> food_rating;
-    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
-        for (int i = 0; i < foods.size(); ++i) {
-            cuisine_ratings[cuisines[i]].insert({ -ratings[i], foods[i] });
-            food_cuisine[foods[i]] = cuisines[i];
-            food_rating[foods[i]] = ratings[i];
+    unordered_map<string, int> rat;
+    unordered_map<string, string> t;
+    unordered_map<string, set<pair<int, string>>> cr;
+    FoodRatings(vector<string>& foods, vector<string>& cuisines,
+                vector<int>& ratings) {
+        for (int i = 0; i < foods.size(); i++) {
+            cr[cuisines[i]].insert({-ratings[i], foods[i]});
+            t[foods[i]] = cuisines[i];
+            rat[foods[i]] = ratings[i];
         }
     }
+
     void changeRating(string food, int newRating) {
-        string cuisine = food_cuisine[food];
-        cuisine_ratings[cuisine].erase({ -food_rating[food], food });
-        cuisine_ratings[cuisine].insert({ -newRating, food });
-        food_rating[food] = newRating;
+        cr[t[food]].erase({-rat[food], food});
+        cr[t[food]].insert({-newRating, food});
+        rat[food] = newRating;
     }
+
     string highestRated(string cuisine) {
-        cout<<cuisine_ratings[cuisine].begin()->second;
-        // pair<int ,string>z=(*cuisine_ratings[cuisine].begin());
-        // cout<<z.second;
-        return (*cuisine_ratings[cuisine].begin()).second;
+        return (*cr[cuisine].begin()).second;
     }
 };
