57574a59d00c8cda1e4c43c16ec2dd6ff28b7962
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-09-21 13:33:06 +0530

    Time: 1219 ms (100.00%) | Memory: 307.7 MB (100.00%) - LeetSync

--- 3928-split-and-merge-array-transformation/split-and-merge-array-transformation.cpp
+++ 3928-split-and-merge-array-transformation/split-and-merge-array-transformation.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minSplitMerge(vector<int>& nums1, vector<int>& nums2) {
+
         queue<pair<int, vector<int>>> q;
 
         q.push({0, nums1});
@@ -20,32 +21,29 @@ public:
                 return count;
             }
 
-            for (int i = 0; i < n; i++) {
-                for (int j = i; j < n; j++) {
-
-                    // Remove this subarray and at at any index k;
-
-                    vector<int> neighbour = v;
-
-                    vector<int> sub(v.begin() + i, v.begin() + j + 1);
-
-                    neighbour.erase(neighbour.begin() + i,
-                                    neighbour.begin() + j + 1);
-
-                    int sz = neighbour.size();
-
-                  
-                    for (int k = 0; k < sz; k++) {
-                        vector<int> next = neighbour;
-                        next.insert(next.begin() + k, sub.begin(), sub.end());
-
+            for (int i = 1; i < n; i++) {
+                for (int j = 0; j <= n - i; j++) {
+                    vector<int> rest = v;
+                    vector<int> sub(v.begin() + j, v.begin() + j + i);
+                    rest.erase(rest.begin() + j, rest.begin() + j + i);
+                    int sz = rest.size();
+                    for (int k = 0; k <= n - i; k++) {
+                        vector<int> next;
+                        for (int x = 0; x < k; x++) {
+                            next.push_back(rest[x]);
+                        }
+                        for (int y = 0; y < sub.size(); y++) {
+                            next.push_back(sub[y]);
+                        }
+                        for (int z = k; z < rest.size(); z++) {
+                            next.push_back(rest[z]);
+                        }
                         if (s.find(next) == s.end()) {
                             s.insert(next);
                             q.push({count + 1, next});
                         }
                     }
 
-                    // Our final array will look like
                 }
             }
         }
