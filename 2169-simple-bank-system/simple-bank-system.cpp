3ef7bbdb68cff0515b337e820b54adc4d04b091b
Shivansh Goel <shivanshgoel1611@gmail.com>
2025-10-27 12:53:21 +0530

    Time: 47 ms (9.18%) | Memory: 150.1 MB (6.49%) - LeetSync

---
+++ 2169-simple-bank-system/simple-bank-system.cpp
@@ -0,0 +1,45 @@
+class Bank {
+public:
+    unordered_map<long long, long long> mp;
+    Bank(vector<long long>& balance) {
+        for (int i = 0; i < balance.size(); i++) {
+            mp[i + 1] = balance[i];
+        }
+    }
+
+    bool transfer(int account1, int account2, long long money) {
+        if (mp.find(account1) == mp.end() || mp.find(account2) == mp.end()) {
+            return false;
+        }
+        if (mp[account1] >= money) {
+            mp[account1] -= money;
+            mp[account2] += money;
+            return true;
+        }
+        return false;
+    }
+
+    bool deposit(int account, long long money) {
+        if (mp.find(account) == mp.end())
+            return false;
+        mp[account] += money;
+        return true;
+    }
+
+    bool withdraw(int account, long long money) {
+        if (mp.find(account) == mp.end())
+            return false;
+        if (mp[account] < money)
+            return false;
+        mp[account] -= money;
+        return true;
+    }
+};
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank* obj = new Bank(balance);
+ * bool param_1 = obj->transfer(account1,account2,money);
+ * bool param_2 = obj->deposit(account,money);
+ * bool param_3 = obj->withdraw(account,money);
+ */
